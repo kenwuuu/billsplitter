7bddb30381d83e70052f3ad4439d79edbe92ab53
kenwu <kenqiwu@gmail.com>
2025-08-09 00:39:01 -0700

    add vue-app folder

---
+++ vue-app/src/App.vue.js
@@ -0,0 +1,27 @@
+import BillSplitter from "@/BillSplitter.vue";
+debugger; /* PartiallyEnd: #3632/scriptSetup.vue */
+const __VLS_ctx = {};
+let __VLS_elements;
+let __VLS_components;
+let __VLS_directives;
+// CSS variable injection 
+// CSS variable injection end 
+/** @type {[typeof BillSplitter, typeof BillSplitter, ]} */ ;
+// @ts-ignore
+const __VLS_0 = __VLS_asFunctionalComponent(BillSplitter, new BillSplitter({}));
+const __VLS_1 = __VLS_0({}, ...__VLS_functionalComponentArgsRest(__VLS_0));
+var __VLS_3 = {};
+var __VLS_2;
+var __VLS_dollars;
+const __VLS_self = (await import('vue')).defineComponent({
+    setup() {
+        return {
+            BillSplitter: BillSplitter,
+        };
+    },
+});
+export default (await import('vue')).defineComponent({
+    setup() {
+    },
+});
+; /* PartiallyEnd: #4569/main.vue */
